5f1778dd8e425f7475f28ad1e8d1a62d801d1287
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2022-09-28 01:22:13 +0200

    SOLVED - Uncaught TypeError: state.auth.currentUser is null

--- src/components/layout/routing/AdminRoutes.jsx
+++ src/components/layout/routing/AdminRoutes.jsx
@@ -1,23 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import PageNotFound from '../../pages/PageNotFound';
 import AdminSidebar from '../sidebar/AdminSidebar';
 
 export default function AdminRoutes() {
-  const currentUser = useSelector(
-    state => state.auth.currentUser.uid === '340y9BTKIAf4Wr2SxWPMCQo6fQR2'
+  const admin = useSelector(
+    state => state.auth.currentUser && state.auth.currentUser.uid === '340y9BTKIAf4Wr2SxWPMCQo6fQR2'
   );
 
-  useEffect(() => {
-    if (!currentUser) {
+  // useEffect(() => {
+  //   if (!admin) {
       
-    }
+  //   }
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentUser]);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [admin]);
 
-  return currentUser ? (
+  return admin ? (
     <AdminSidebar>
       <Outlet />
     </AdminSidebar>
