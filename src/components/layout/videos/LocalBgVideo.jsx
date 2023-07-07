f1945cbf03f65ab2e0ef11d03bf4d714bfdba6d1
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2023-07-07 23:44:40 +0200

    adding new components to home page

--- src/components/layout/videos/LocalBgVideo.jsx
+++ src/components/layout/videos/LocalBgVideo.jsx
@@ -1,96 +1,77 @@
-import { AbsoluteCenter, Box, Flex, Heading, Text } from '@chakra-ui/react';
+import { Box, Center, Heading, Text } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import React from 'react';
 
-const BackgroundVideo = () => {
-    const videoUrl = 'https://firebasestorage.googleapis.com/v0/b/matteo-dirollo-com.appspot.com/o/Videos%2Flosange.mp4?alt=media&token=9f972aec-d7ca-4a07-b946-c12f7fd54cb1'
+const LocalBgVideo = () => {
+  const videoUrl =
+    'https://firebasestorage.googleapis.com/v0/b/matteo-dirollo-com.appspot.com/o/Videos%2Flosange.mp4?alt=media&token=9f972aec-d7ca-4a07-b946-c12f7fd54cb1';
+
   return (
     <Box
       pos="relative"
-      top={0}
-      left={0}
       width="100%"
-      height="100%"
-      zIndex={-1}
+      minHeight={['20vh', '40vh', '60vh', '100vh']}
       overflow="hidden"
-      sx={{
-        width: '100%',
-        height: '100%',
-        zIndex: '-1',
-        transition: 'opacity 0.2s ease',
-        '.youtube-background': { opacity: '5' },
-        '.youtube-background.fade-out': { opacity: '0' },
-        '.youtube-background.fade-out .player': {
-          pointerEvents: 'none',
-        },
-      }}
-      minHeight="100vh"
-      marginTop="0"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
     >
-
-      <motion.video
-        autoPlay
-        loop
-        muted
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 1 }}
-        w="100%"
-        h="100%"
-        objectFit="cover"
-      >
-        <source
-          src={videoUrl}
-          type="video/mp4"
-        />
-        
-      </motion.video>
-      <Flex
+      <Box
         pos="absolute"
         top={0}
         left={0}
-        w="100%"
-        h="100%"
-        justify="center"
-        align="center"
-        zIndex={1}
+        width="100%"
+        height="100%"
+        zIndex={-1}
       >
-         <Box w="100%" minHeight="100vh" marginTop="0">
-          <AbsoluteCenter borderRadius="12px" >
-            <Heading
-              lineHeight={1.1}
-              fontWeight={600}
-              fontSize={{ base: '3xl', sm: '4xl', lg: '8xl' }}
-              color={'white'}
-              p={8}
+        <motion.video
+          autoPlay
+          loop
+          muted
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          width="100%"
+          height="100%"
+          objectFit="cover"
+        >
+          <source src={videoUrl} type="video/mp4" />
+        </motion.video>
+      </Box>
+      <Center zIndex={1}>
+        <Box maxW={['300','450','600px']}>
+          <Heading
+            lineHeight={1.1}
+            fontWeight={600}
+            fontSize={{ base: '3xl', sm: '4xl', lg: '8xl' }}
+            color="white"
+          >
+            <Text
+              as="span"
+              position="relative"
+              _after={{
+                content: "''",
+                width: 'full',
+                height: '30%',
+                position: 'absolute',
+                bottom: 1,
+                left: 0,
+                bg: 'teal.400',
+                zIndex: -1,
+              }}
             >
-              <Text
-                as={'span'}
-                position={'relative'}
-                _after={{
-                  content: "''",
-                  width: 'full',
-                  height: '30%',
-                  position: 'absolute',
-                  bottom: 1,
-                  left: 0,
-                  bg: 'teal.400',
-                  zIndex: -1,
-                }}
-              >
-                Freelance
-              </Text>
-              <br />
-              <Text as={'span'} color={'white'}>
-                Graphic Designer & Creative
-              </Text>
-            </Heading>
-          </AbsoluteCenter>
+              Freelance
+            </Text>
+            <br />
+            <Text as="span" color="white">
+              Graphic Designer & Creative
+            </Text>
+          </Heading>
         </Box>
-      </Flex>
+      </Center>
     </Box>
   );
 };
 
-export default BackgroundVideo;
+export default LocalBgVideo;
