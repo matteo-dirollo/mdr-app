9df63be158996294a44d6dbf15f9181e09b149f8
matteo-dirollo <matteo.dirollo@accademiabellearti.fr.it>
2023-07-11 01:11:45 +0200

    Styling the contact form

--- src/components/layout/forms/TextareaInput.jsx
+++ src/components/layout/forms/TextareaInput.jsx
@@ -5,20 +5,31 @@ import {
   FormLabel,
   FormErrorMessage,
   Textarea,
-  useColorModeValue
+  useColorModeValue,
 } from '@chakra-ui/react';
 
 export default function TextareaInput({ errors, label, ...props }) {
   const [field, meta] = useField(props);
   return (
     <FormControl isInvalid={meta.touched && !!meta.error}>
-      <FormLabel color={useColorModeValue('gray.700', 'gray.100')} marginTop="12px">{label}</FormLabel>
+      <FormLabel
+        color={useColorModeValue('gray.700', 'gray.100')}
+        marginTop="12px"
+      >
+        {label}
+      </FormLabel>
       <Textarea
-        borderColor="gray.300"
-        _hover={{
-          borderRadius: 'gray.300',
+        variant="filled"
+        sx={{
+          borderRadius: 0,
+          borderWidth: '1px',
+          // borderColor: 'gray.300',
+          _focus: {
+            borderColor: 'teal.300',
+            borderWidth: '2px', // Change the border weight for the :focus state
+          },
         }}
-        placeholder="message"
+        // placeholder="Text here..."
         {...field}
         {...props}
       />
--- src/theme.js
+++ src/theme.js
@@ -1,31 +1,46 @@
-import { extendTheme } from "@chakra-ui/react";
-import { buttonTheme } from "./styles/buttonTheme";
+import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
-    fonts: {
-      heading: "Epilogue, sans-serif",
-      body: "Epilogue, sans-serif",
+  fonts: {
+    heading: 'Epilogue, sans-serif',
+    body: 'Epilogue, sans-serif',
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 500,
+    bold: 700,
+  },
+  fontSizes: {
+    xs: '0.75rem',
+    sm: '0.875rem',
+    md: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.5rem',
+    '3xl': '1.875rem',
+    '4xl': '2.25rem',
+    '5xl': '3rem',
+    '6xl': '4rem',
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        borderRadius: 0,
+      },
     },
-    fontWeights: {
-      normal: 400,
-      medium: 500,
-      bold: 700,
+    Input: {
+      baseStyle: {
+        borderRadius: 0,
+        borderWidth: '2px',
+        borderColor: 'gray.300',
+        _focus: {
+          borderColor: 'teal.500',
+          borderWidth: '4px', // Change the border weight for the :focus state
+          boxShadow: 'outline',
+        },
+      },
     },
-    fontSizes: {
-      xs: "0.75rem",
-      sm: "0.875rem",
-      md: "1rem",
-      lg: "1.125rem",
-      xl: "1.25rem",
-      "2xl": "1.5rem",
-      "3xl": "1.875rem",
-      "4xl": "2.25rem",
-      "5xl": "3rem",
-      "6xl": "4rem",
-    },
-    components: {
-      Button: buttonTheme
-    }
-  });
+  },
+});
 
 export default theme;
